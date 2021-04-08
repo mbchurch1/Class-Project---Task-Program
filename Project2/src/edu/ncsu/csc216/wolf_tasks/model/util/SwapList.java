2630f0f3b5d2ec8c2fb90939d1230099518d882a
wsgoodwi <wsgoodwi@ncsu.edu>
2021-04-07 20:24:22 -0400

    [Implementation] All classes and test classes are implemented in skeleton form

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
@@ -6,9 +6,105 @@ package edu.ncsu.csc216.wolf_tasks.model.util;
  * @author Matthew Church
  * @author Will Goodwin
  * @author John Firlet
+ * @param <E>
  *
  */
-public class SwapList {
+public class SwapList<E> {
+	
+	/** initial capacity of the SwapList */
+	private static final int INITIAL_CAPACITY = 10;
+	/** list of generic elements */
+	private E[] list;
+	/** size of the SwapList */ 
+	private int size;
+	
+	/**
+	 * Constructor for SwapList
+	 */
+	public SwapList() {
+		
+	}
+	
+	/**
+	 * Adds an element to the SwapList
+	 * @param element the element to be added to the SwapList
+	 */
+	public void add(E element) {
+		
+	}
+	
+	/**
+	 * Checks the capacity of the SwapList
+	 * @param cap the capacity of the list
+	 */
+	private void checkCapacity(int cap) {
+		
+	}
+	
+	
+	/**
+	 * Removes the element at the parameter index
+	 * @param index the index of the element to be removed
+	 */
+	public void remove(int index) {
+		
+	}
+	
+	
+	/**
+	 * Checks whether the index is legal for this list
+	 * @param index the index of an element in the SwapList
+	 */
+	private void checkIndex(int index) {
+		
+	}
+	
+	/**
+	 * Moves an element up in priority in the SwapList
+	 * @param index the index of the element to move
+	 */
+	public void moveUp(int index) {
+		
+	}
 
-
+	/**
+	 * Moves an element down in priority in the SwapList
+	 * @param index the index of the element to move
+	 */
+	public void moveDown(int index) {
+		
+	}
+	
+	/**
+	 * Moves an element to front of list
+	 * @param index the index of the element to move
+	 */
+	public void moveToFront(int index) {
+		
+	}
+	
+	/**
+	 * Moves an element to back of list
+	 * @param index the index of the element to move
+	 */
+	public void moveToBack(int index) {
+		
+	}
+	
+	/**
+	 * Gets an element in the list
+	 * @param index the index of the element to get
+	 */
+	public void get(int index) {
+		
+	}
+	
+	/**
+	 * Gets the size of the list
+	 * @return the size of the list
+	 */
+	public int size() {
+		return 0;
+	}
+	
 }
--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SwapListTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SwapListTest.java
@@ -1,8 +1,12 @@
+/**
+ * 
+ */
 package edu.ncsu.csc216.wolf_tasks.model.util;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
 /**
  * tests SwapList
  * 
@@ -12,12 +16,77 @@ import org.junit.Test;
  *
  */
 public class SwapListTest {
+
 	/**
-	 * tests SwapList
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#SwapList()}.
 	 */
 	@Test
 	public void testSwapList() {
-		fail("Not yet implemented"); // TODO
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#add(java.lang.Object)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#remove(int)}.
+	 */
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#moveUp(int)}.
+	 */
+	@Test
+	public void testMoveUp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#moveDown(int)}.
+	 */
+	@Test
+	public void testMoveDown() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#moveToFront(int)}.
+	 */
+	@Test
+	public void testMoveToFront() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#moveToBack(int)}.
+	 */
+	@Test
+	public void testMoveToBack() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#get(int)}.
+	 */
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.wolf_tasks.model.util.SwapList#size()}.
+	 */
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
 	}
 
 }
