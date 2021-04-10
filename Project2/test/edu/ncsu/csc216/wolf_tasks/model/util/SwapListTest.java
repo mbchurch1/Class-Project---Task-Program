7c8d33e151a7f971782aa221538244a8e3636bbe
John Firlet <jmfirlet@ncsu.edu>
2021-04-10 15:34:47 -0400

    Adding unfinished SwapListTest.  Need to double check the constructor since it should create a list with capacity of 10. Also need to test growArray and checkCapacity.

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SwapListTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SwapListTest.java
@@ -17,28 +17,67 @@ import org.junit.Test;
  */
 public class SwapListTest {
 
+	/** list of generic elements */
+	private SwapList testList;
+	
+	
 	/**
 	 * Test method for swapList()
 	 */
 	@Test
 	public void testSwapList() {
-		fail("Not yet implemented");
+		//Test the constructor and add methods
+		SwapList<Object> list = new SwapList<Object>();
+		assertEquals(0, list.size());
+		list.add("Test");
+		list.add("Test2");
+		list.add("Test3");
+		assertEquals(3, list.size());
+		//Test add with a null value
+		try {
+			list.add(null);
+		} catch (NullPointerException e) {
+			assertEquals("Cannot add null element.", e.getMessage());
+			}
+		
+		//Constructor should create a list of capacity 10
+		//Need to test growArray and checkCapacity
+//		testList = new SwapList();
+//		assertEquals(10, testList.size());
+		
+		
 	}
 
-	/**
-	 * Test method for add()
-	 */
-	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
-	}
 
 	/**
 	 * Test method for remove()
 	 */
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
+		//Test the remove method
+		SwapList<Object> list = new SwapList<Object>();
+		assertEquals(0, list.size());
+		list.add("Test");
+		list.add("Test2");
+		list.add("Test3");
+		assertEquals(3, list.size());
+		assertEquals("Test2", list.remove(1));
+		assertEquals(2, list.size());
+		assertEquals("Test3", list.get(1));
+		//Test remove with an invalid index
+		SwapList<Object> list2 = new SwapList<Object>();
+		try {
+			list.remove(0);
+		} catch (IndexOutOfBoundsException e) {
+			assertEquals("Invalid index.", e.getMessage());
+			}
+		list2.add("Test");
+		try {
+			list2.remove(5);
+		} catch (IndexOutOfBoundsException e) {
+			assertEquals("Invalid index.", e.getMessage());
+			}
+		
 	}
 
 	/**
@@ -46,7 +85,17 @@ public class SwapListTest {
 	 */
 	@Test
 	public void testMoveUp() {
-		fail("Not yet implemented");
+		//Test the moveUp method
+		SwapList<Object> list = new SwapList<Object>();
+		assertEquals(0, list.size());
+		list.add("Test");
+		list.add("Test2");
+		list.add("Test3");
+		assertEquals(3, list.size());
+		list.moveUp(2);
+		assertEquals("Test3", list.get(1));
+		list.moveUp(1);
+		assertEquals("Test3", list.get(0));
 	}
 
 	/**
@@ -54,7 +103,18 @@ public class SwapListTest {
 	 */
 	@Test
 	public void testMoveDown() {
-		fail("Not yet implemented");
+		//Test the moveDown method
+		SwapList<Object> list = new SwapList<Object>();
+		assertEquals(0, list.size());
+		list.add("Test");
+		list.add("Test2");
+		list.add("Test3");
+		assertEquals(3, list.size());
+		list.moveDown(0);
+		assertEquals("Test", list.get(1));
+		list.moveDown(1);
+		assertEquals("Test", list.get(2));
+		
 	}
 
 	/**
@@ -62,7 +122,17 @@ public class SwapListTest {
 	 */
 	@Test
 	public void testMoveToFront() {
-		fail("Not yet implemented");
+		//Test the moveToFront method
+		SwapList<Object> list = new SwapList<Object>();
+		assertEquals(0, list.size());
+		list.add("Test");
+		list.add("Test2");
+		list.add("Test3");
+		assertEquals(3, list.size());
+		list.moveToFront(2);
+		assertEquals("Test3", list.get(0));
+		list.moveToFront(2);
+		assertEquals("Test2", list.get(0));
 	}
 
 	/**
@@ -70,23 +140,39 @@ public class SwapListTest {
 	 */
 	@Test
 	public void testMoveToBack() {
-		fail("Not yet implemented");
+		//Test the moveToFront method
+		SwapList<Object> list = new SwapList<Object>();
+		assertEquals(0, list.size());
+		list.add("Test");
+		list.add("Test2");
+		list.add("Test3");
+		assertEquals(3, list.size());
+		list.moveToBack(0);
+		assertEquals("Test", list.get(2));
+		list.moveToBack(0);
+		assertEquals("Test2", list.get(2));
 	}
 
 	/**
 	 * Test method for get()
 	 */
 	@Test
-	public void testGet() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for size()
-	 */
-	@Test
-	public void testSize() {
-		fail("Not yet implemented");
+	public void testGrowArray() {
+		SwapList<Object> list = new SwapList<Object>();
+		assertEquals(0, list.size());
+		list.add("Test");
+		list.add("Test2");
+		list.add("Test3");
+		list.add("Test4");
+		list.add("Test5");
+		list.add("Test6");
+		list.add("Test7");
+		list.add("Test8");
+		list.add("Test9");
+		list.add("Test10");
+		list.add("Test11");
+		list.add("Test12");
+		assertEquals(12, list.size());
 	}
 
 }
