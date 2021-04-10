a0b22f99f3c29c1753a43369633ae28602f35515
John Firlet <jmfirlet@ncsu.edu>
2021-04-10 13:16:32 -0400

    Adding incomplete SortedList class.  Cannot add "implements ISortedList" and compareTo to the add method.

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SortedList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SortedList.java
@@ -1,6 +1,7 @@
 package edu.ncsu.csc216.wolf_tasks.model.util;
 
 
+
 /**
  * SortedList constructor
  * 
@@ -14,46 +15,104 @@ public class SortedList<E> {
 	
 	/** Number of elements in the SortedList */
 	private int size;
+	/** ListNode for front of the list */
+	private ListNode front;
 	
 	/**
 	 * Constructor for SortedList
 	 */
 	public SortedList() {
-		//added to remove SA notif
-		size += 1;
+		front = null;
+		size = 0;
 	}
 	
 	/**
 	 * Adds an element to the SortedList
 	 * @param object the object being added to the SortedList
+	 * @throws NullPointerException if element is null
+	 * @throws IllegalArgumentException if element cannot be added 
 	 */
 	public void add(E object) {
+		ListNode current = front; 
+		if(object == null) {
+			throw new NullPointerException("Cannot add null element.");
+		}
+		if(size == 0) {
+			front = new ListNode(object, front);
+		} else {
+			for(int i = 0; i < size; i++) {
+				if(current.data.equals(object)) {
+					throw new IllegalArgumentException("Cannot add duplicate element");
+				}
+				current = current.next;
+			}
+		}
+		
+//		current = front;
+//		for(int i = 0; i < size; i++) {
+//			if(((E) current.next.data).comparableTo(object) {
+//				
+//			}
+//		}
+//		
+//		try {
+//			
+//		} catch(IllegalArgumentException e) {
+//			throw new IllegalArgumentException("")
+//		}
 		
 	}
 	
 	/**
-	 * Removes an object from the SortedList
-	 * @param index the index of the element being removed from the SortedList
-	 * @return 0 as a placeholder;
+	 * Returns the element from the given index.  The element is
+	 * removed from the list.
+	 * @param index index to remove element from
+	 * @return element at given index
+	 * @throws IndexOutOfBoundsException if the index is out of bounds
+	 * 		for the list
 	 */
-	public int remove(int index) {
-		return 0;
+	public E remove(int index) {
+		checkIndex(index);
+		
+		E value =  null;
+		if(index == 0) {
+			value = (E) front.data;
+			front = front.next;
+		} else {
+			ListNode current = front;
+			for(int i = 0; i < index - 1; i++) {
+				current = current.next;
+			}
+			value = (E) current.next.data;
+			current.next = current.next.next;
+		}
+		size--;
+		return value;
 	}
 	
 	/**
 	 * Checks if there is an element at the provided index
 	 * @param index the index to check
+	 * @throws IndexOutOfBoundsException if index is less than 0 or greater than size
 	 */
 	private void checkIndex(int index) {
-		
+		if(index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Invalid index.");
+		}
 	}
 	
 	/**
 	 * Determines whether the SortedList contains the parameterized object
 	 * @param object an object that may be contained within the SortedList
-	 * @return true or false based on whether the SortedList contains object
+	 * @return true or false based on whether the SortedList contains the given object
 	 */
 	public boolean contains(E object) {
+		ListNode current = front;
+		for(int i = 0; i < size; i++) {
+			if(current.data.equals(object)) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
@@ -61,9 +120,17 @@ public class SortedList<E> {
 	 * Gets the object at the parameterized index from the SortedList
 	 * @param index the index of the object being returned
 	 * @return e  An object at the given index in the SortedList
+	 * @throws IndexOutOfBoundsException if the index is out of bounds
+	 * 		for the list
 	 */
 	public E get(int index) {
-		return null;
+		checkIndex(index);
+		ListNode current = front;
+		for(int i = 0; i < size; i++) {
+			current = current.next;
+		}
+		
+		return (E) current.data;
 	}
 	
 	/**
@@ -71,6 +138,25 @@ public class SortedList<E> {
 	 * @return the size of the list
 	 */
 	public int size() {
-		return 0;
+		return size;
+	}
+	
+	
+	public class ListNode<E> {
+		
+		/**Private data field for the current ListNode  */
+		public E data;
+		/** Next node on the list   */
+		private ListNode next;
+		
+		/**
+		 * Constructor for the ListNode
+		 * @param data  Element data
+		 * @param node  ListNode 
+		 */
+		public ListNode(E data, ListNode next) {
+			this.data = data;
+			this.next = next;
+		}
 	}
 }
