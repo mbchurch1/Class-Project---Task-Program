61bc4e192db331e5db5238dfbec9697d29fc5848
John Firlet <jmfirlet@ncsu.edu>
2021-04-14 20:55:55 -0400

    Reverting back to original NotebookReader.

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
@@ -1,18 +1,12 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
-
 import java.io.File;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
-
-
 import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
-
 /**
  * Reads Notebooks from file
  * 
@@ -23,6 +17,12 @@ import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
  */
 public class NotebookReader {
 
+
+	/** String representing a recurring task in the notebook file */
+	private static final String RECURRING = "recurring"; 
+	/** String representing an active task in the notebook file */
+	private static final String ACTIVE = "active"; 
+
 	/**
 	 * reads a notebook from a file
 	 * 
@@ -30,35 +30,39 @@ public class NotebookReader {
 	 * @return noteBook A Notebook object
 	 */
 	public static Notebook readNodebookFile(File file) {
-		Notebook notebook = null;
-		String nbReader = "";
+		if (file == null) {
+			throw new IllegalArgumentException("File does not exist.");
+		} 
+		
+		String entireFile = "";
+		Notebook notebook = new Notebook("Current Notebook");
+		
 		try {
-			Scanner fileReader = new Scanner(new FileInputStream(file));
-			if(fileReader.next() != "!") {
-				throw new IllegalArgumentException("Unable to load file.");
-			}
-			
+			Scanner fileReader = new Scanner(new FileInputStream(file)); 
 			while(fileReader.hasNextLine()) {
-				nbReader += fileReader.nextLine() + "\n";
+				entireFile += fileReader.nextLine() + "\n";
 			}
 			fileReader.close();
-		} catch(FileNotFoundException e) {
-			throw new IllegalArgumentException("Unable to load file.");
-		}
-	
-		Scanner notebookReader = new Scanner(nbReader);
-		notebookReader.useDelimiter("\\r?\\n?[#]");
-		String noteBook = null;
-		while(notebookReader.hasNext()) {
-			noteBook = notebookReader.next();
-			notebook = new Notebook(noteBook);
-			notebook.addTaskList(processTaskList(notebookReader.next()));
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("File cannot be read: " + file);
 		}
 		
-		notebookReader.close();
+		if(entireFile.charAt(0) != '!') {
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		
+		String taskListToken = "";
+		Scanner taskListScanner = new Scanner(entireFile).useDelimiter("\\r?\\n?[#]");
+		
+		while (taskListScanner.hasNext()) {
+			taskListToken = taskListScanner.next();
+			TaskList taskList = processTaskList(taskListToken);
+			notebook.addTaskList(taskList);
+		}
+		
+		taskListScanner.close();
 		return notebook;
 	}
-
 	/**
 	 * Processes a TaskList read in from a Notebook file to achieve proper
 	 * formatting
@@ -67,18 +71,40 @@ public class NotebookReader {
 	 * @return taskList A TaskList object
 	 */
 	private static TaskList processTaskList(String list) {
-		Scanner taskListReader = new Scanner(list);
-		String taskListName = taskListReader.nextLine().trim();
-		
-		TaskList newTaskList = new TaskList(taskListName, 0);
-		taskListReader.useDelimiter("\\r?\\n?[*]");
-		while(taskListReader.hasNext()) {
-			newTaskList.addTask(processTask(newTaskList, taskListReader.next().trim()));
-		}
-		taskListReader.close();
-		return newTaskList;
-	}
+		String listName = "";
+		int numCompletedTasks = 0;
+		Scanner lineScanner = new Scanner(list);
 
+		String nameAndCompletions = lineScanner.nextLine();
+		//String trimmedNameAndCompletions = nameAndCompletions.trim();
+		Scanner nameVsCompletionsScanner = new Scanner(nameAndCompletions).useDelimiter(",");
+		listName = nameVsCompletionsScanner.next();
+		String trimmedName = listName.trim();
+		if(nameVsCompletionsScanner.hasNextInt()) {
+			numCompletedTasks = nameVsCompletionsScanner.nextInt();
+		}
+		
+		String taskListString = "";
+		
+		while(lineScanner.hasNextLine()) {
+			taskListString += lineScanner.nextLine() + "\n";
+		}
+
+		TaskList taskList = new TaskList(trimmedName, numCompletedTasks);
+
+		Scanner taskTokenScanner = new Scanner(taskListString).useDelimiter("\\r?\\n?[*]");
+		while (taskTokenScanner.hasNext()) {
+			String taskToken = taskTokenScanner.next();
+			Task taskForTaskList = processTask(taskList, taskToken);
+			taskList.addTask(taskForTaskList);
+		}
+		
+		lineScanner.close();
+		nameVsCompletionsScanner.close();
+		taskTokenScanner.close();
+		
+		return taskList;
+	}
 	/**
 	 * Process a Task read in from Notebook file that is part of a particular
 	 * TaskList, formatting the task before returning
@@ -88,29 +114,49 @@ public class NotebookReader {
 	 *                   Notebook file
 	 * @return task A Task object
 	 */
-	private static Task processTask(AbstractTaskList list, String taskString) {
-		Scanner taskReader = new Scanner(taskString);
-		String task = taskReader.nextLine();
-		Scanner taskScanner = new Scanner(task);
-		taskScanner.useDelimiter(",");
-		Task newTask = null;
-		String taskName = null;
-		String taskDescription = null;
-		String isRecurring = null;
-		String isActive = null;
-		while(taskScanner.hasNext()) {
-			taskName = taskScanner.next();
-			isRecurring = taskScanner.next();
-			isActive = taskScanner.next();
+	private static Task processTask(AbstractTaskList taskList, String taskToken) {
+		String taskName = "";
+		String taskDescription = ""; 
+		boolean isRecurring = false; 
+		boolean isActive = false;
+
+		Scanner taskTokenScanner = new Scanner(taskToken);
+
+		String taskNameAndModifiers = taskTokenScanner.nextLine();
+		while(taskTokenScanner.hasNextLine()) {
+			taskDescription += taskTokenScanner.nextLine() + "\n";
 		}
-		while(taskReader.hasNext()) {
-			taskDescription += taskReader.next();
+
+		Scanner taskNameAndModsScanner = new Scanner(taskNameAndModifiers);
+
+		String recurringOrActive1 = "";
+		String recurringOrActive2 = "";
+
+		if(taskNameAndModsScanner.hasNext()) {
+			taskName = taskNameAndModsScanner.next().trim();
 		}
-		newTask = new Task(taskName, taskDescription, Boolean.parseBoolean(isRecurring), Boolean.parseBoolean(isActive));
-		
-		
-		taskReader.close();
-		taskScanner.close();
+		if(taskNameAndModsScanner.hasNext()) {
+			recurringOrActive1 = taskNameAndModsScanner.next();
+			if (recurringOrActive1.equals(RECURRING)) {
+				isRecurring = true;
+			} else if (recurringOrActive1.equals(ACTIVE)) {
+				isActive = true;
+			} 
+		}
+		if(taskNameAndModsScanner.hasNext()) {
+			recurringOrActive2 = taskNameAndModsScanner.next();
+			if (recurringOrActive2.equals(RECURRING)) {
+				isRecurring = true;
+			} else if (recurringOrActive2.equals(ACTIVE)) {
+				isActive = true;
+			} 
+		}
+
+		Task newTask = new Task(taskName, taskDescription, isRecurring, isActive);
+
+		taskTokenScanner.close();
+		taskNameAndModsScanner.close();
+
 		return newTask;
 	}
 }
