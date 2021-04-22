b0cb05561fb1765d1b3d95acf1471923b14f9b70
wsgoodwi <wsgoodwi@ncsu.edu>
2021-04-22 12:55:03 -0400

    [Debugging] Commented out the contents of printTest

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/printTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/printTest.java
@@ -6,15 +6,15 @@ import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
 
 public class printTest {
 
-	public static void main(String[] args) {
-		NotebookReader reader1 = new NotebookReader();
-		
-		Notebook notebook1 = null;
-		
-		File notebook1File = new File("test-files/notebook1.txt");
-		
-		notebook1 = reader1.readNodebookFile(notebook1File);
-
-	}
+//	public static void main(String[] args) {
+//		NotebookReader reader1 = new NotebookReader();
+//		
+//		Notebook notebook1 = null;
+//		
+//		File notebook1File = new File("test-files/notebook1.txt");
+//		
+//		notebook1 = reader1.readNodebookFile(notebook1File);
+//
+//	}
 
 }
